f1a080da7488d0e883c5bd61ae27fbd4427b4828
Jonathan Bugge <jonathan_bugge1@baylor.edu>
2021-11-17 17:20:34 -0600

    what is happening

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request): #Get and post are the two type of requests
+    return render(request, 'MainApp/index.html')
+    
---
+++ MyShell.py
@@ -0,0 +1,24 @@
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", 'learning_log.settings')
+
+import django
+django.setup()
+
+from MainApp.models import Topic, Entry
+
+topics = Topic.objects.all() #equivalent of Select * from in SQL
+
+for topic in topics:
+    print(topic.id)
+    print(topic.text) #can also do just (Topic) because of the str code we have in models.
+    print(topic.date_added)  #all from models
+
+
+t = Topic.objects.get(id = 1)
+print(t) #prints Chess
+
+entries = t.entry_set.all()
+
+
+for e in entries:
+    print(e)
