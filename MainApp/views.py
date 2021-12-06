f2cd62f6029230e174141cd8a1dee783c01a4b0d
Jonathan Bugge <jonathan_bugge1@baylor.edu>
2021-12-06 16:04:24 -0600

    slides 8-12

--- MainApp/views.py
+++ MainApp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 
 from .forms import EntryForm, TopicForm
 
-from .models import Topic
+from .models import Topic, Entry
 
 
 # Create your views here.
@@ -62,3 +62,17 @@ def new_entry(request, topic_id):
 
     return render(request, 'MainApp/new_entry.html', context) # conetxt is a dictionary        
 
+def edit_entry(request, entry_id):
+    '''Edit and exisitn entry'''
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != 'POST':
+        form = EntryForm(instance=entry)
+    else:
+        form = EntryForm(instance=entry, data = request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('MainApp:topic', topic_id = topic.id)
+    context = {'entry': entry, 'topic': topic, 'form': form}
+    return render(request, 'MainApp/edit_entry.html', context)
