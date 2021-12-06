fb6852b361e7817fe2a222cd18e3b298d6b7d62f
Jonathan Bugge <jonathan_bugge1@baylor.edu>
2021-12-06 16:54:43 -0600

    created first user/ registration page

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-
+from . import views # . means same folder
 app_name = 'users'
 
 urlpatterns = [
     path('',include('django.contrib.auth.urls')),
+    path('register/',views.register,name='register'),
 ]
